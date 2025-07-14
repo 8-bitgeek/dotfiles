53372893915673a70840250a2f9113d0e7f4fadd
zhaozengqiang <zhaozq@csdn.net>
2025-07-14 15:14:50 +0800

    update cursor configuration

--- zsh/09-vi.zsh
+++ zsh/09-vi.zsh
@@ -1,5 +1,5 @@
-bindkey '^v' edit-command-line
-bindkey -v
+bindkey '^v' edit-command-line              # 将 Ctrl+v 绑定为 edit-command-line
+bindkey -v                                  # 启用 vi 模式
 
 # bindkey -M vicmd "i" vi-insert
 # bindkey -M vicmd "K" vi-insert-bol
@@ -15,24 +15,26 @@ bindkey -v
 # bindkey -M vicmd "h" vi-forward-word-end
 
 function zle-keymap-select {
+    # $KEYMAP 是当前键盘的映射模式
 	if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
-		echo -ne '\e[1 q'
+		echo -ne '\e[2 q'                   # 进入 normal 模式时, 方块
 	elif [[ ${KEYMAP} == main ]] || [[ ${KEYMAP} == viins ]] || [[ ${KEYMAP} = '' ]] || [[ $1 = 'beam' ]]; then
-		echo -ne '\e[5 q'
+		echo -ne '\e[4 q'                   # 处于 insert 模式时, 下划线
   fi
 }
-zle -N zle-keymap-select
+
+zle -N zle-keymap-select                    # 将函数注册为钩子函数, 每次在 insert/normal 切换时执行 zle-keymap-select
 
 # Use beam shape cursor on startup.
-echo -ne '\e[5 q'
+echo -ne '\e[4 q'
 
 # Use beam shape cursor for each new prompt.
 preexec() {
-	echo -ne '\e[5 q'
+	echo -ne '\e[4 q'
 }
 
 _fix_cursor() {
-	echo -ne '\e[5 q'
+	echo -ne '\e[4 q'
 }
 precmd_functions+=(_fix_cursor)
 
