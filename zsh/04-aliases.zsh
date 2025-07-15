fcd2cb3ac1e4cd654de7deb8b0f53d5dd4e798dc
zhaozengqiang <zhaozq@csdn.net>
2025-07-15 11:46:08 +0800

    add alias for pbcopy

--- zsh/04-aliases.zsh
+++ zsh/04-aliases.zsh
@@ -1,8 +1,7 @@
 alias ta='tmux att -t'
 alias tn='tmux new -s'
 alias tl='tmux ls'
-alias ssg='ssh root@gldwolf.cn'
-alias sso='ssh ubuntu@oracle'
 alias pof='unset all_proxy https_proxy http_proxy'
 alias vim='nvim'
+alias pc='pbcopy'
 # alias bt=blueutil --connect "$(blueutil --paired | grep -B1 'not connected' | grep 'name:' | cut -d'"' -f2 | head -1)"
