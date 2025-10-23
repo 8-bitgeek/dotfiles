a61a9e1352f09fb1ad432d0283b935ede4b1f36f
zengqiangz <zengqiangz@tujia.com>
2025-10-23 21:42:32 +0800

    feat: add key binding for Home/End

--- zsh/06-keybindings.zsh
+++ zsh/06-keybindings.zsh
@@ -1,6 +1,18 @@
 # 默认使用 vi 模式
 bindkey -v
 
+# 在 insert 模式中，绑定 Home/End 键
+bindkey -M viins '^[[H' beginning-of-line
+bindkey -M viins '^[OH' beginning-of-line  # 有的终端发这个序列
+bindkey -M viins '^[[F' end-of-line
+bindkey -M viins '^[OF' end-of-line
+
+# 在 normal 模式中, 绑定 Home/End 键
+bindkey -M vicmd '^[[H' beginning-of-line
+bindkey -M vicmd '^[OH' beginning-of-line
+bindkey -M vicmd '^[[F' end-of-line
+bindkey -M vicmd '^[OF' end-of-line
+
 # 上下方向键支持 substring 搜索
 zmodload -F zsh/terminfo +p:terminfo
 for key ('^[[A' '^P' ${terminfo[kcuu1]}) bindkey ${key} history-substring-search-up
--- zsh/deploy.sh
+++ zsh/deploy.sh
@@ -21,29 +21,29 @@ echo "▶️ Deploying zsh dotfiles from $DOTFILES_DIR"
 # Step 1: 创建 config 目录
 mkdir -p "$ZSH_CONFIG_DIR"
 
-# Step 2: 备份老文件
-backup_if_exists "$ZSHRC_TARGET"
-backup_if_exists "$ZIMRC_TARGET"
-
-# Step 3: 链接 .zshrc 和 .zimrc
-ln -sf "$DOTFILES_DIR/zshrc" "$ZSHRC_TARGET"
-ln -sf "$DOTFILES_DIR/zimrc" "$ZIMRC_TARGET"
-echo "✅ Linked zshrc & zimrc"
-
-# Step 4: 链接 *.zsh 文件
-for file in "$DOTFILES_DIR"/*.zsh; do
-    filename=$(basename "$file")
-    ln -sf "$file" "$ZSH_CONFIG_DIR/$filename"
-    echo "✅ Linked $filename → $ZSH_CONFIG_DIR/"
-done
-
-# Step 5: 安装 zimfw（如果没装）
+# Step 2: 安装 zimfw（如果没装）
 if [ ! -f "$HOME/.zim/zimfw.zsh" ]; then
     echo "📦 Installing zimfw..."
     curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 else
     echo "✅ zimfw 已安装"
 fi
+
+# step 3: 备份老文件
+backup_if_exists "$ZSHRC_TARGET"
+backup_if_exists "$ZIMRC_TARGET"
+
+# Step 4: 链接 .zshrc 和 .zimrc
+ln -sf "$DOTFILES_DIR/zshrc" "$ZSHRC_TARGET"
+ln -sf "$DOTFILES_DIR/zimrc" "$ZIMRC_TARGET"
+echo "✅ Linked zshrc & zimrc"
+
+# Step 5: 链接 *.zsh 文件
+for file in "$DOTFILES_DIR"/*.zsh; do
+    filename=$(basename "$file")
+    ln -sf "$file" "$ZSH_CONFIG_DIR/$filename"
+    echo "✅ Linked $filename → $ZSH_CONFIG_DIR/"
+done
 
 echo "✅ 部署完成。你可以执行："
 echo "   source ~/.zshrc"
